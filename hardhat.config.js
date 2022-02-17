be43d92f72d05dc96903c21da899b87046f8fdbf
markvillanueva08 <monkeytheluffy08@gmail.com>
2022-02-17 13:11:20 +0800

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
-
-
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
 module.exports = {
+  defaultNetworks: "matic",
+  networks:{
+    hardhat:{
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [privateKey]
+    },
+    mainet: {
+      url: "https://rpc-mainnet.matic.network",
+      accounts: [privateKey]
+    },
+
+  },
+
   solidity: "0.8.4",
 };
